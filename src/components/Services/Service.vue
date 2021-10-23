be502268e9886e230cde591f597f6e7f12c9d8e7
SheikhRashed <devrashed06@gmail.com>
2021-10-23 20:56:24 +0600

    accordion added

--- src/components/Services/Service.vue
+++ src/components/Services/Service.vue
@@ -1,8 +1,46 @@
 <template lang="">
-    <div>
+    <div class="relative py-24">
         <!-- Service Wrapper Start  -->
-        <div class="container">
-            <h2>Hello I'm Service Section</h2>
+        <div class="container ">
+
+            <!-- left start  -->
+            <div>
+                <!-- Service Info start  -->
+                <h6>YOu need transparency</h6>
+                <h2>We've got you covered</h2>
+                <p>in publishing and grapich design, lorem ipsum is a placeholder text commonly used to demonstrate</p>
+                <!-- Service Info End  -->
+
+                <!-- Accordion start  -->
+                <vue-collapsible-panel-group>
+    <vue-collapsible-panel>
+        <template #title>
+            Panel A Title
+        </template>
+        <template #content>
+            Panel A Content
+        </template>
+    </vue-collapsible-panel>
+    <vue-collapsible-panel>
+        <template #title>
+            Panel B Title
+        </template>
+        <template #content>
+            Panel B Content
+        </template>
+    </vue-collapsible-panel>
+    ...
+</vue-collapsible-panel-group>
+                <!-- Accordion end  -->
+
+            </div>
+            <!-- left end  -->
+
+            <!-- right start  -->
+            <div>
+
+            </div>
+            <!-- right end  -->
         </div>
         <!-- Service Wrapper End  -->
     </div>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import VueCollapsiblePanel from '@dafcoe/vue-collapsible-panel';
+import '@dafcoe/vue-collapsible-panel/dist/vue-collapsible-panel.css';
 import './assets/css/index.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(VueCollapsiblePanel).mount('#app');
+// createApp(App).mount('#app');
