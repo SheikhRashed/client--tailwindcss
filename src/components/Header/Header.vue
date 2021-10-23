30915aeb74dc2cfce67de940be566865be598dec
SheikhRashed <devrashed06@gmail.com>
2021-10-23 14:04:40 +0600

    sidebar created

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,13 +1,13 @@
 <template lang="">
-    <header class="relative overflow-hidden bg-white mb-12 mt-6 lg:mb-24 lg:mt-8">
+    <header class="relative  bg-white mb-12 mt-6 lg:mb-24 lg:mt-8" :class="{ 'scrolled-nav': scrollPosition}">
         <!-- Container  -->
-        <div class="container flex justify-between items-center">
+        <nav class="container flex justify-between items-center">
             <!-- left content   -->
             <div class="flex items-center">
                 <!-- Brand Logo  -->
                 <a href="#" class="text-brandLogo font-logo uppercase mr-14"> logo </a>
                 <!-- menu -->
-                <ul class="hidden xl:flex items-center space-x-12">
+                <ul class="hidden xl:flex items-center space-x-12" v-show="!mobile">
                     <li>
                         <a href="#" class="text-menu uppercase font-primary-medium">About us</a>
                     </li>
@@ -27,21 +27,65 @@
             </div>
             <!-- right content -->
             <div>
-                <div class="hidden xl:flex items-center space-x-4">
+                <div class="hidden xl:flex items-center space-x-4" v-show="!mobile">
                     <a href="#" class="text-menu uppercase font-primary-medium">Enterprise</a>
                     <a href="#" class="btn btn-outline">Login</a>
                     <a href="#" class="btn btn-primary">equip now</a>
                 </div>
                 <a href="#" class="inline-block bg-primary px-4 py-2 text-white text-2xl rounded-lg xl:hidden">
-                    <i class="fas fa-bars"></i>
+                    <i @click="toggleMobileNav" v-show="mobile" class="fas fa-bars" :class="{'icon-active' : mobileNav}" ></i>
                 </a>
             </div>
-        </div>
+            <transition name="mobile-nav" class="bg-primary py-12  w-ful">
+                  <ul class=" xl:flex flex-col items-center space-y-12 absolute z-10 top-24 left-0 text-center w-full" v-show="mobileNav">
+                    <li>
+                        <a href="#" class="text-menu uppercase font-primary-medium">About us</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-menu uppercase font-primary-medium">Benefits</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-menu uppercase font-primary-medium">Service</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-menu uppercase font-primary-medium">Pricing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-menu uppercase font-primary-medium">Resources</a>
+                    </li>
+                    <li class="flex justify-center space-x-4">
+                      <a href="#" class="btn btn-outline border-white text-black">Login</a>
+                      <a href="#" class="btn btn-outline border-white text-black">equip now</a>
+                    </li>
+                 
+                </ul>
+            </transition>
+        </nav>
     </header>
+
 </template>
 <script>
+
+import Sidebar from '../Sidebar/Sidebar.vue';
+
     export default {
         name: "Header",
+        // components: { 
+        //   Sidebar
+        // }
+        data() { 
+          return { 
+            scrollPosition: null,
+            mobile: false,
+            mobileNav: false,
+            windowWidth: null,
+          }
+        }
+
     };
 </script>
-<style lang=""></style>
+<style lang="scss" scoped>
+
+
+
+</style>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <!-- Sidbar Wrapper  -->
+  <div>
+      <h2>Sidebar</h2>
+  </div>
+</template>
+<script>
+export default {
+    name: 'Sidebar'
+}
+</script>
+<style lang="">
+    
+</style>
