f20624267554faee3592b71ed412409f91bfbb52
SheikhRashed <devrashed06@gmail.com>
2021-10-24 16:27:37 +0600

    our sotry responsive fixed

--- src/components/OurStory.vue
+++ src/components/OurStory.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="relative py-[190px]">
+    <div class="relative py-16 lg:py-[190px]">
       <!-- Connect Wrapper  -->
         <div class="container">
           <!-- our-story-content -->
@@ -8,16 +8,16 @@
             <!-- story-slider-wrapper -->
             <div class="story-slider-wrapper">
               <!-- story-slider-item -->
-              <div class="flex">
-                <img src="../assets/images/story-image.jpg" class="w-1/3 max-w-full mr-8" alt="">
-                <div>
-                  <h3 class="text-8xl mb-8 font-primary-regular">Our Story</h3>
-                  <p class="text-3xl leading-10 font-primary-medium tracking-wider">In publishing and graphic design, Lorem ipsum is a <br/> placeholder text commonly used to demonstrate the visual.</p>
+              <div class="flex flex-col lg:flex-row ">
+                <img src="../assets/images/story-image.jpg" class=" w-full  h-96 lg:w-1/3 max-w-full mr-8 object-cover" alt="slider-img">
+                <div class="mt-6 md:mt-12 lg:mt-0">
+                  <h3 class=" text-4xl md:text-6xl lg:text-8xl leading-none mb-4 lg:mb-8 font-primary-medium text-gray-700 ">Our Story</h3>
+                  <p class=" text-md md:text-2xl lg:text-3xl leading-normal lg:leading-10 font-secondary-regular tracking-wider">In publishing and graphic design, Lorem ipsum is a <br/> placeholder text commonly used to demonstrate the visual.</p>
                 </div>
               </div>
             </div>
             <!-- story-slider-arrow -->
-            <ul class="m-0 flex justify-end mt-9">
+            <ul class="m-0 flex justify-end mt-4 md:mt-9">
               <li>
                 <button type="button" class="w-10 h-10 flex items-center justify-center border text-gray-600 border-gray-600 mr-2">
                   <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polyline points="15 18 9 12 15 6"></polyline></svg>
